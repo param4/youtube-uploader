5116a9f0cf042904ee13206234d084af9c132317
Parminder Singh <parminder@gamechangesns.com>
2024-08-10 19:29:44 +0530

    feat: adds youtube module skeleton

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { YoutubeModule } from './youtube/youtube.module';
 import BaseConfig from 'config/base.config';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
     load: [BaseConfig]
-  })],
+  }), YoutubeModule],
   controllers: [AppController],
   providers: [AppService],
 })
